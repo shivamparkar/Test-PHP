399d761c457431cd98dec2e93e8a58727c1e5f21
Shivam Parkar <70848428+shivamparkar@users.noreply.github.com>
2021-09-06 21:51:38 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,109 @@
+<footer class="main-footer alternet-5">
+        <div class="footer-upper">
+            <div class="auto-container">
+                <div class="upper-inner clearfix">
+                    <div class="text pull-left">
+                        <h2>Do you want to learn about us?</h2>
+                        <p>Consectetur adipisicing elit sed eiusmod tempor incididunt labore dolore magna aliqua.</p>
+                    </div>
+                    <div class="btn-box pull-right">
+                        <a href="index-5.html" class="theme-btn style-one">Get In Touch Today</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-top">
+            <div class="auto-container">
+                <div class="widget-section">
+                    <div class="row clearfix">
+                        <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
+                            <div class="footer-widget logo-widget">
+                                <figure class="footer-logo"><a href="index.html"><img src="assets/images/footer-logo.png" alt=""></a></figure>
+                                <div class="text">
+                                    <p>Tempor incididunt ut labore eut dolore veniam quis nostrud exercitation ullamc consequat. Duis aute irure.</p>
+                                </div>
+                                <ul class="info-list clearfix">
+                                    <li><i class="fas fa-map-marker-alt"></i>838 Andy Street, Madison, NJ 08003</li>
+                                    <li><i class="fas fa-envelope"></i>Email <a href="mailto:support@my-domain.com">support@my-domain.com</a></li>
+                                    <li><i class="fas fa-headphones"></i>Support <a href="tel:01005200369">0100 5200 369</a></li>
+                                </ul>
+                                <ul class="social-links clearfix">
+                                    <li><a href="index.html"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="index.html"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="index.html"><i class="fab fa-instagram"></i></a></li>
+                                    <li><a href="index.html"><i class="fab fa-linkedin-in"></i></a></li>
+                                    <li><a href="index.html"><i class="fab fa-pinterest-p"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
+                            <div class="footer-widget links-widget ml-70">
+                                <div class="widget-title">
+                                    <h4>Useful Links</h4>
+                                </div>
+                                <div class="widget-content">
+                                    <ul class="list clearfix">
+                                        <li><a href="index.html">About Us</a></li>
+                                        <li><a href="index.html">What We Offers</a></li>
+                                        <li><a href="index.html">Testimonials</a></li>
+                                        <li><a href="index.html">Our Projectss</a></li>
+                                        <li><a href="index.html">Latest News</a></li>
+                                        <li><a href="index.html">Privacy Policy</a></li>
+                                        <li><a href="index.html">Contact Us</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
+                            <div class="footer-widget links-widget">
+                                <div class="widget-title">
+                                    <h4>What We Do</h4>
+                                </div>
+                                <div class="widget-content">
+                                    <ul class="list clearfix">
+                                        <li><a href="index.html">Financial Advice</a></li>
+                                        <li><a href="index.html">Business Planning</a></li>
+                                        <li><a href="index.html">Startup Help</a></li>
+                                        <li><a href="index.html">Investment Strategy</a></li>
+                                        <li><a href="index.html">Management Services</a></li>
+                                        <li><a href="index.html">Market Research</a></li>
+                                        <li><a href="index.html">SEO Optimization</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
+                            <div class="footer-widget newsletter-widget">
+                                <div class="widget-title">
+                                    <h4>Newslette</h4>
+                                </div>
+                                <div class="widget-content">
+                                    <div class="text">
+                                        <p>Get in your inbox the latest News</p>
+                                    </div>
+                                    <form action="http://azim.commonsupport.com/Fionca/contact.html" method="post" class="newsletter-form">
+                                        <div class="form-group">
+                                            <i class="far fa-user"></i>
+                                            <input type="text" name="name" placeholder="Your Name" required="">
+                                        </div>
+                                        <div class="form-group">
+                                            <i class="far fa-envelope"></i>
+                                            <input type="email" name="email" placeholder="Email address" required="">
+                                        </div>
+                                        <div class="form-group message-btn">
+                                            <button type="submit">subscribe</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <div class="auto-container">
+                <div class="copyright"><p>&copy; 2020 <a href="index.html">FIONCA</a> - Business & Consulting. All rights reserved.</p></div>
+            </div>
+        </div>
+    </footer>
---
+++ includes/header.php
@@ -0,0 +1,176 @@
+<header class="main-header style-one style-six">
+        <div class="header-top">
+            <div class="auto-container">
+                <div class="top-inner clearfix">
+                    <ul class="info top-left pull-left">
+                        <li><i class="fas fa-map-marker-alt"></i>838 Andy Street, Madison, NJ 08003</li>
+                        <li><i class="fas fa-headphones"></i>Support <a href="tel:01005200369">0100 5200 369</a></li>
+                    </ul>
+                    <div class="top-right pull-right">
+                        <ul class="social-links clearfix">
+                            <li><a href="index.html"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="index.html"><i class="fab fa-google-plus-g"></i></a></li>
+                            <li><a href="index.html"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="index.html"><i class="fab fa-linkedin-in"></i></a></li>
+                            <li><a href="index.html"><i class="fab fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header-lower">
+            <div class="auto-container">
+                <div class="outer-box clearfix">
+                    <div class="logo-box pull-left">
+                        <figure class="logo"><a href="index.php"><img src="assets/images/logo-6.png" alt=""></a></figure>
+                    </div>
+                    <div class="menu-area pull-right">
+                        <!--Mobile Navigation Toggler-->
+                        <div class="mobile-nav-toggler">
+                            <i class="icon-bar"></i>
+                            <i class="icon-bar"></i>
+                            <i class="icon-bar"></i>
+                        </div>
+                        <nav class="main-menu navbar-expand-md navbar-light">
+                            <div class="collapse navbar-collapse show clearfix" id="navbarSupportedContent">
+                                <ul class="navigation clearfix">
+                                    <li class="dropdown"><a href="about-1.php">Home</a>
+                                        <ul>
+                                            <li><a href="index.html">Home Page One</a></li>
+                                            <li><a href="index-2.html">Home Page Two</a></li>
+                                            <li><a href="index-3.html">Home Page Three</a></li>
+                                            <li><a href="index-4.html">Home Page Four</a></li>
+                                            <li><a href="index-5.html">Home Page Five</a></li>
+                                            <li><a href="index-onepage.html">Home OnePage</a></li>
+                                            <li class="dropdown"><a href="about-1.php">Header Style</a>
+                                                <ul>
+                                                    <li><a href="index.html">Header Style One</a></li>
+                                                    <li><a href="index-2.html">Header Style Two</a></li>
+                                                    <li><a href="index-3.html">Header Style Three</a></li>
+                                                    <li><a href="index-4.html">Header Style Four</a></li>
+                                                    <li><a href="index-5.html">Header Style Five</a></li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </li> 
+                                    <li class="current dropdown"><a href="about-1.php">About</a>
+                                        <ul>
+                                            <li><a href="about-1.html">About Us 01</a></li>
+                                            <li><a href="about-2.html">About Us 02</a></li>
+                                            <li><a href="team.html">Experts Team</a></li>
+                                            <li><a href="pricing.html">Our Pricing</a></li>
+                                            <li><a href="error.html">Error Page</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="dropdown"><a href="about-1.php">Services</a>
+                                        <ul>
+                                            <li><a href="service-1.html">Service Page 01</a></li>
+                                            <li><a href="service-2.html">Service Page 02</a></li>
+                                            <li><a href="financial-analysis.html">Financial Analysis</a></li>
+                                            <li><a href="taxation-planning.html">Taxation Planning</a></li>
+                                            <li><a href="investment-trading.html">Investment Trading</a></li>
+                                            <li><a href="wealth-marketing.html">Wealth Marketing</a></li>
+                                            <li><a href="planning-strategies.html">Planning Strategies</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="dropdown"><a href="about-1.php">Elements</a>
+                                        <div class="megamenu">
+                                            <div class="row clearfix">
+                                                <div class="col-lg-3 column">
+                                                    <ul>
+                                                        <li><h4>Elements 1</h4></li>
+                                                        <li><a href="feature-element-1.html">Feature 01</a></li>
+                                                        <li><a href="feature-element-2.html">Feature 02</a></li>
+                                                        <li><a href="feature-element-3.html">Feature 03</a></li>
+                                                        <li><a href="about-element-1.html">About 01</a></li>
+                                                        <li><a href="about-element-2.html">About 02</a></li>
+                                                        <li><a href="about-element-3.html">About 03</a></li>
+                                                        <li><a href="about-element-4.html">About 04</a></li>
+                                                        <li><a href="stats-element.html">Stats Element</a></li>
+                                                    </ul>
+                                                </div>
+                                                <div class="col-lg-3 column">
+                                                    <ul>
+                                                        <li><h4>Elements 2</h4></li>
+                                                        <li><a href="news-element-1.html">News 01</a></li>
+                                                        <li><a href="news-element-2.html">News 02</a></li>
+                                                        <li><a href="funfact-element-1.html">Fun Fact 01</a></li>
+                                                        <li><a href="funfact-element-2.html">Fun Fact 02</a></li>
+                                                        <li><a href="service-element-1.html">Service 01</a></li>
+                                                        <li><a href="service-element-2.html">Service 02</a></li>
+                                                        <li><a href="skills-element.html">Skills Element</a></li>
+                                                        <li><a href="clients-element.html">Clients</a></li>
+                                                    </ul>
+                                                </div> 
+                                                <div class="col-lg-3 column">
+                                                    <ul>
+                                                        <li><h4>Elements 3</h4></li>
+                                                        <li><a href="team-element-1.html">Team 01</a></li>
+                                                        <li><a href="team-element-2.html">Team 02</a></li>
+                                                        <li><a href="pricing-element.html">Pricing</a></li>
+                                                        <li><a href="testimonial-element-1.html">Testimonial 01</a></li>
+                                                        <li><a href="testimonial-element-2.html">Testimonial 02</a></li>
+                                                        <li><a href="testimonial-element-3.html">Testimonial 03</a></li>
+                                                        <li><a href="work-element-1.html">Working 01</a></li>
+                                                        <li><a href="work-element-2.html">Working 02</a></li>
+                                                    </ul>
+                                                </div> 
+                                                <div class="col-lg-3 column">
+                                                    <ul>
+                                                        <li><h4>Elements 4</h4></li>
+                                                        <li><a href="project-element-1.html">Project 01</a></li>
+                                                        <li><a href="project-element-2.html">Project 02</a></li>
+                                                        <li><a href="chart-element.html">Chart Element</a></li>
+                                                        <li><a href="footer-element-1.html">Footer 01</a></li>
+                                                        <li><a href="footer-element-2.html">Footer 02</a></li>
+                                                        <li><a href="footer-element-3.html">Footer 03</a></li>
+                                                        <li><a href="footer-element-4.html">Footer 04</a></li>
+                                                        <li><a href="footer-element-5.html">Footer 05</a></li>
+                                                    </ul>
+                                                </div>                                       
+                                            </div>                                            
+                                        </div>
+                                    </li>
+                                    <li class="dropdown"><a href="about-1.html">Blog</a>
+                                        <ul>
+                                            <li><a href="blog-grid.html">Blog Grid</a></li>
+                                            <li><a href="blog-classic.html">Blog Classic</a></li>
+                                            <li><a href="blog-details.html">Blog Details</a></li>
+                                        </ul>
+                                    </li>                              
+                                    <li><a href="contact.php">Contact</a></li>
+                                </ul>
+                            </div>
+                        </nav>
+                        <div class="menu-right-content clearfix">
+                            <div class="search-btn">
+                                <button type="button" class="search-toggler"><i class="flaticon-search-1"></i></button>
+                            </div>
+                            <div class="nav-btn nav-toggler navSidebar-button clearfix">
+                                <i class="fas fa-align-right"></i>
+                            </div>
+                            <div class="btn-box">
+                                <a href="about-1.html" class="theme-btn style-one">Free Estimate</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--sticky Header-->
+        <div class="sticky-header">
+            <div class="auto-container">
+                <div class="outer-box clearfix">
+                    <div class="logo-box pull-left">
+                        <figure class="logo"><a href="index.html"><img src="assets/images/small-logo.png" alt=""></a></figure>
+                    </div>
+                    <div class="menu-area pull-right">
+                        <nav class="main-menu clearfix">
+                            <!--Keep This Empty / Menu will come through Javascript-->
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
